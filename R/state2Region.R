a58e094fc65b3a8e1f48bbbd95961c6972a60429
ashata <asha.myid@gmail.com>
2018-04-02 00:56:03 -0500

    r

--- R/state2Region.R
+++ R/state2Region.R
@@ -15,8 +15,8 @@ states <- cbind.data.frame(Abbr = state.abb, Name = state.name, Region = state.r
 #' groupStatesToRegions
 groupStatesToRegions <- function(states){
   states <- state2abbrV2(states)
-  regions <- lapply(states, toRegions)
-  return(regions)
+  states <- lapply(states, toRegions)
+  return(states)
 }
 
 #library(tidyverse)
@@ -25,9 +25,7 @@ groupStatesToRegions <- function(states){
 toRegions <- function(x){
   if(is.element(x, states$Abbr)){
     filteredState <- states %>% filter(Abbr == x)
-    region <- filteredState[1,3]
-    print(region)
-    return(region)
+    return(as.character(filteredState[1,3]))
   }
   else{
     return(x)
